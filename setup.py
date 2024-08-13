39be831de1bd2f3714375fc0785ec3e8c3543150
Ka Hei Choi <darkfire9152@gmail.com>
2024-08-13 10:03:03 +0200

    Create setup.py
    Setup for pip

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="grfcsc",
+    version="0.1.0",
+    packages=[''grf_qftcs'],
+    install_requires=[
+        "numpy",     
+        "matplotlib",
+        "scipy",
+        "cupy",
+    ],
+    author="Ka Hei Choi",
+    author_email="K.Choi@physik.uni-muenchen.de",
+    description="A package for Gaussian Random Field simulations and statistics analysis in De Sitter space and Radiation Dominated Universe.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/khchoi-lmu-physik/grf_qftcs", 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    keywords="random field simulation"
+)
